b14fffe9792323a0d059e6ca35694afb958073b9
omercsx <omercagris@gmail.com>
2025-02-07 06:29:56 -0500

    Add database connection to Express server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const connectDB = require("./src/config/database");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
